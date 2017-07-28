c3951a6b631cc4ebc013e755c695278a93cfbc7f
Diana A. Balan <diana.balan@telenav.com>
2017-07-28 11:54:30 +0300

    Simple news website which retrieves news from DB

---
+++ application/views/pages/home.php
@@ -0,0 +1,5 @@
+
+This is the home page for the simple learning friday news app. Feel free to navigate to these pages:
+
+<p><a href="<?php echo site_url('news'); ?>">All news</a></p>
+<p><a href="<?php echo site_url('about'); ?>">About</a></p>
